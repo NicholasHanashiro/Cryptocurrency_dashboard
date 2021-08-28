0b114239476d57654d417424674c6405c4dd0ebb
Justin Huang <jmhuang1995@gmail.com>
2021-08-28 10:55:32 -0700

    Add files via upload

--- bars.js
+++ bars.js
@@ -20,27 +20,55 @@ function init () {
             if (data[i].Ticker == 'BTC') {
                 y = data[i].twitter_followers;
                 z = data[i].reddit_subscribers;
+                x = data[i].Price;
             };
             if (!Cryptos.includes(data[i].Name)){
                 Cryptos.push(data[i].Name);
             };
         };
 
+        var yVals = [y,z]
         var trace1 = {
             x: ['Twitter Followers', 'Reddit Subscribers'],
-            y: [y, z],
+            y: yVals,
             type: 'bar',
             name: 'BTC',
+            text: yVals,
+            textposition:'auto',
+            marker: {
+              color: 'red',
+              opacity: 0.6
+            }
+        };
+        var xVals = [x]
+        var trace2 = {
+          x: ['Price'],
+          y: xVals,
+          type: 'bar',
+          name: 'BTC',
+          text: xVals,
+          textposition:'auto',
+          marker: {
+            color: 'green',
+            opacity: 0.6
+          }
         };
 
         var barData = [trace1];
+        var barData2 = [trace2];
 
         var layout = {
             title:'Bitcoin',
             yaxis: {title: 'Followers for Coin'}
         };
 
+        var layout2 = {
+          title: 'Bitcoin',
+          yaxis: {title: 'Price'}
+        };
+
         Plotly.newPlot("bar", barData, layout);
+        Plotly.newPlot("bar2", barData2,layout2);
 
         var dropDown = d3.select("#selDataset");
         for (var i = 0; i < Cryptos.length; i++) {
@@ -66,18 +94,26 @@ function newCoin() {
                 newz = data[i].reddit_subscribers;
                 var chart_title = data[i].Name
                 var chart_ticker = data[i].Ticker
+                var price = data[i].Price
             };
         };
 
-        updatePlot(newy, newz, chart_title, chart_ticker);
+        updatePlot(newy, newz, chart_title, chart_ticker,price);
     });
 
 }
 
-function updatePlot( newy,newz, chart_title, chart_ticker) {
+function updatePlot( newy,newz, chart_title, chart_ticker,price) {
     Plotly.restyle("bar", "y", [[newy, newz]]);
     Plotly.restyle("bar", "name", chart_ticker);
+    Plotly.restyle("bar","text",[[newy,newz]]);
     Plotly.relayout("bar", "title", chart_title);
+
+    Plotly.restyle("bar2","y",[[price]]);
+    Plotly.restyle("bar2", "name", chart_ticker);
+    Plotly.restyle("bar2","text",[[price]]);
+    Plotly.relayout("bar2", "title", chart_title);
+
 }
 
 init();
