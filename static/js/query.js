dda42836abe62ae189f4167476d9f18d89f35838
Mack D <mckenna.duvall@gmail.com>
2021-08-28 13:52:34 -0700

    styling update

--- static/js/query.js
+++ static/js/query.js
@@ -19,39 +19,76 @@ function runEnter() {
     d3.event.preventDefault();
     var inputValue = input.property("value");
 
-var base_url = "https://api.coingecko.com/api/v3/coins/"
+    var base_url = "https://api.coingecko.com/api/v3/coins/"
 
-var end_url = "?tickers=true&market_data=true"
+    var end_url = "?tickers=true&market_data=true"
 
-var url = base_url + inputValue + end_url
+    var url = base_url + inputValue + end_url
 
+    //make a fetch requests
+    fetch(url)
 
-//make a fetch requests
-fetch(url)
+        .then(response => {
+            return response.json();
+        }).then(json => {
 
-    .then(response=>{
-        return response.json();
-    }).then(json=> {
+            console.log(json)
 
-        cryptoArray = Object.values(json);
+            cryptoArray = Object.values(json);
 
-        coin = cryptoArray[11]
+            coin = cryptoArray[11]
 
-        // console.log(coin[11])
+            // console.log(coin[11])
 
-        coinDescriptions = Object.values(coin)
+            coinDescriptions = Object.values(coin)
 
-        coinDescription = coinDescriptions[0]
+            coinDescription = coinDescriptions[0]
 
-        console.log(coinDescription)
+            console.log(coinDescription)
 
-    var coinDesc = d3.select("#cryptoDesc").text(coinDescription)
-
-    
-
+            var coinDesc = d3.select("#cryptoDesc").text(coinDescription)
 
 
-    });
+        });
 };
 
 
+
+window.onload = function runPage() {
+
+    price_url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+
+    cryptoList = ['bitcoin', 'ethereum', 'cardano', 'binancecoin', 'ripple', 'dogecoin', 'solana', 'polkadot', 'uniswap', 'terra-luna']
+
+    priceContainter = d3.select("#prices")
+
+
+    fetch(price_url)
+
+        .then(response => {
+            return response.json();
+        }).then(json => {
+
+            // console.log(json)
+
+            json.forEach(function (item, index, array) {
+                // console.log(item)
+
+                cryptoId = item.id
+                price = item.current_price
+
+                // console.log(cryptoId)
+                // console.log(price)
+
+                if (cryptoList.includes(cryptoId)) {
+
+                    priceContainter.append("h4").text(price)
+
+                    console.log(cryptoId)
+                    console.log(price)
+                };
+
+            });
+
+        });
+};
